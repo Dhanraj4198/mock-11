77a3b6ce2549d7f79df16dd51cd9caa233478e03
Dhanraj Jagtap <107112212+Dhanraj4198@users.noreply.github.com>
2023-01-19 12:55:52 +0530

    Fixed: Multiple Signup Attempts

--- routes/signup.routes.js
+++ routes/signup.routes.js
@@ -9,23 +9,24 @@ signupRouter.post("/", async (req, res) => {
   const userPresent = await UserModel.findOne({ email });
   if (userPresent) {
     res.send("Try loggin in, user already exist");
-  }
-  try {
-    bcrypt.hash(password, 4, async function (err, hash) {
-      if (err) {
-        console.log(err);
-      } else {
-        const user = new UserModel({ email, password: hash });
-        await user.save();
-        res.send("signup successfull");
-      }
-    });
-  } catch (err) {
-    console.log(err);
+  } else {
+    try {
+      bcrypt.hash(password, 4, async function (err, hash) {
+        if (err) {
+          console.log(err);
+        } else {
+          const user = new UserModel({ email, password: hash });
+          await user.save();
+          res.send("signup successfull");
+        }
+      });
+    } catch (err) {
+      console.log(err);
+    }
   }
 });
 signupRouter.get("/data", async (req, res) => {
-    let users = await UserModel.find();
-    res.send(users);
-  });
+  let users = await UserModel.find();
+  res.send(users);
+});
 module.exports = { signupRouter };
